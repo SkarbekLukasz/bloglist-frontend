250971f66ae33eba160b54c533d69badadd77f5b
Luke <nycthbre@gmail.com>
2024-07-04 19:41:53 +0200

    Exercise 5.1 completion (bloglist-frontend)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
+import Blogs from './components/Blogs'
 import blogService from './services/blogs'
+import Login from './components/Login'
+import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +18,41 @@ const App = () => {
     )  
   }, [])
 
+  const handleUsernameChange = (event) => {
+    const changedUsername = event.target.value
+    setUsername(changedUsername)
+  }
+
+  const handlePasswordChange = (event) => {
+    const changedPassword = event.target.value
+    setPassword(changedPassword)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({username, password})
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <h1>Blogs list</h1>
+      <Notification message={errorMessage}/>
+      {user === null ?
+       <Login handleLogin={handleLogin} handlePasswordChange={handlePasswordChange} handleUsernameChange={handleUsernameChange}/> :
+       <Blogs blogs={blogs} user={user}/>}
     </div>
   )
+
 }
 
 export default App
