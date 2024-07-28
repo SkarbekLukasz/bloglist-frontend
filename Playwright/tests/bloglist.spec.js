ce50b33be51439422816f30f49897b89b8103477
Luke <nycthbre@gmail.com>
2024-07-28 16:39:38 +0200

    Exercise 5.18 completion (playwright)

--- Playwright/tests/bloglist.spec.js
+++ Playwright/tests/bloglist.spec.js
@@ -1,12 +1,36 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
+const { loginWith } = require("./helper");
 
 describe("Blog list app E2E tests", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post("http://localhost:5173/api/testing/reset");
+    await request.post("http://localhost:5173/api/users", {
+      data: {
+        name: "Matti Luukkainen",
+        username: "mluukkai",
+        password: "salainen",
+      },
+    });
+
     await page.goto("http://localhost:5173");
   });
 
   test("Login form is shown", async ({ page }) => {
     const loginButton = await page.getByText("login");
     await expect(loginButton).toBeVisible();
+  });
+
+  describe("Login", () => {
+    test("Succeeds with correct credentials", async ({ page }) => {
+      await loginWith(page, "mluukkai", "salainen");
+
+      await expect(page.getByText("Matti Luukkainen logged in")).toBeVisible();
+    });
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await loginWith(page, "mlukai", "salainen");
+
+      await expect(page.getByText("Wrong credentials")).toBeVisible();
+    });
   });
 });
---
+++ Playwright/tests/helper.js
@@ -0,0 +1,7 @@
+const loginWith = async (page, username, password) => {
+  await page.getByRole("textbox").first().fill(username);
+  await page.getByRole("textbox").last().fill(password);
+  await page.getByRole("button", { name: "login" }).click();
+};
+
+export { loginWith };
